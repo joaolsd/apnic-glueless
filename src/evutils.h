80e1970fb515744a3abceafecb90f339ad4735bb
Joao Damas <joao@apnic.net>
2020-04-06 11:04:54 +0000

    Sad, but forgot to add function declaration to header file evutils.h

--- src/evutils.h
+++ src/evutils.h
@@ -29,6 +29,7 @@ void query_check(evldns_server_request *srq, void* /* user_data */,
 				 ldns_rr_class qclass);
 
 void truncation_check(evldns_server_request *srq);
+void sibling_truncation_check(evldns_server_request *srq);
 
 #ifdef __cplusplus
 }
